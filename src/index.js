9a59f584c3c1374a5ef92bf2e514b52aa62934ce
swdrana <tctr2s@gmail.com>
2022-06-26 00:39:00 +0600

    backend worked perfectly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
 import "./index.css";
 // import App from './App';
@@ -10,27 +11,20 @@ import "./index.css";
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
+  .use(HttpApi)
   .init({
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next",
-        },
-      },
-      bn: {
-        translation: {
-          "Welcome to React": "React এবং react-i18next এ আপনাকে স্বাগতম",
-        },
-      },
-    },
     fallbackLng: "en",
-    detection : {
-      order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
-      caches: ['cookie','localStorage'],
-    }
+    detection: {
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      caches: ["cookie", "localStorage"],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: {useSuspense:false}
   });
 
 function App() {
