b010788796d43e0317c950db20bc0d2aa75b480d
swdrana <tctr2s@gmail.com>
2022-06-26 10:20:24 +0600

    show loading... when data is load

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import i18n from "i18next";
-import {initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import App from "./App";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    supportedLngs:['en','bn'],
+    supportedLngs: ["en", "bn"],
 
     // the translations
     // (tip move them in a JSON file and import them,
@@ -24,16 +24,15 @@ i18n
       caches: ["cookie"],
     },
     backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
+      loadPath: "/assets/locales/{{lng}}/translation.json",
     },
-    react: {useSuspense:false}
   });
-
-
-
+const loadingMarkup = <div className="py-4 text-center"> <h2>Loading...</h2> </div>
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById("root")
 );
